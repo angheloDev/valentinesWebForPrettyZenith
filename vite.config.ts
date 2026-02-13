654d8c948912abc092f368fa7c241dabc86c12c2
angheloDev <120046334+angheloDev@users.noreply.github.com>
2026-02-13 20:51:38 +0800

    fix napud

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/valentinesWebForPrettyZenith',
+	base: '/will-you-be-my-valentine/',
 	plugins: [react()],
 });
